29789b6e305c1cda7da12094a9e676ce2c2629fc
Vansladd <92092512+Vansladd@users.noreply.github.com>
2023-03-21 20:42:03 +0000

    Add files via upload

---
+++ market maker.py
@@ -0,0 +1,130 @@
+import math
+import betfairlightweight
+from betfairlightweight import filters
+
+class MarketMaker:
+    def __init__(self, trd, batb, batl):
+        self.trd = trd
+        self.batb = batb
+        self.batl = batl
+        self.spread = 0.02
+        self.client = betfairlightweight.APIClient(username='your_username',
+                                                   password='your_password',
+                                                   app_key='your_app_key',
+                                                   cert_file='your_ssl_cert.pem',
+                                                   key_file='your_ssl_key.pem')
+    
+    def calc_implied_prob(self, odds):
+        return 1 / odds
+    
+    def calc_implied_odds(self, prob):
+        return 1 / prob
+    
+    def calc_kelly_bet_size(self, implied_prob, actual_prob, odds):
+        kelly_f = (implied_prob - actual_prob) / (odds - 1)
+        return kelly_f
+    
+    def get_best_orders(self):
+        best_bid = self.batb[0][1]
+        best_ask = self.batl[0][1]
+        for order in self.batb:
+            if order[1] > best_bid:
+                best_bid = order[1]
+        for order in self.batl:
+            if order[1] < best_ask:
+                best_ask = order[1]
+        return (best_bid, best_ask)
+    
+    def make_market(self, num_ticks=5):
+        best_bid, best_ask = self.get_best_orders()
+        mid_price = (best_bid + best_ask) / 2
+        tick_size = (best_ask - best_bid) / (num_ticks - 1)
+        
+        # calculate implied probabilities for each tick
+        probs = []
+        for i in range(num_ticks):
+            price = best_bid + i * tick_size
+            prob = self.calc_implied_prob(price)
+            probs.append(prob)
+        
+        # use Markov Chain model to predict match outcome probabilities
+        predicted_probs = markov_chain_predictor.predict()
+        
+        # calculate optimal bet sizes using Kelly Criterion
+        optimal_bets = []
+        for i in range(num_ticks):
+            actual_prob = predicted_probs[i]
+            kelly_f = self.calc_kelly_bet_size(probs[i], actual_prob, mid_price)
+            optimal_bets.append(kelly_f)
+        
+        # place orders
+        for i in range(num_ticks):
+            price = best_bid + i * tick_size
+            size = optimal_bets[i] * self.trd[0][1] # using the volume of the last trade as the size
+            if price >= mid_price:
+                back_price = price
+                lay_price = back_price + self.spread
+                back_size = size
+                lay_size = size * (mid_price - self.spread - back_price) / (mid_price - back_price)
+                self.place_order('BACK', back_price, back_size)
+                self.place_order('LAY', lay_price, lay_size)
+            else:
+                lay_price = price
+                back_price = lay_price - self.spread
+                lay_size = size
+                back_size = size * (lay_price - mid_price - self.spread) / (lay_price - mid_price)
+                self.place_order('LAY', lay_price, lay_size)
+                self.place_order('BACK', back_price, back_size)
+                
+    def place_order(self, side, price, size):
+        if side == 'BACK':
+            order_type = 'LIMIT'
+            bet_type = 'B'
+        elif side == 'LAY':
+            order_type = 'LIMIT'
+            bet_type = 'L'
+        else:
+            raise ValueError("Invalid side, must be 'BACK' or 'LAY'")
+        
+        bet_size = round(size, 2)  # round to two decimal places
+        bet_price = round(price, 2)  # round to two decimal places
+        
+        market_id = '1.23456789'  # replace with actual market ID
+        selection_id = '12345678'  # replace with actual selection ID
+        
+        response = self.client.betting.place_order(market_id=market_id,
+                                                    selection_id=selection_id,
+                                                    side=bet_type,
+                                                    order_type=order_type,
+                                                    limit_order=betfairlightweight.filters.price_ladder_limit_order(
+                                                        price=bet_price,
+                                                        size=bet_size))
+        
+        if response.status == 'SUCCESS':
+            print(f"Order placed: {side} {bet_size}@{bet_price}")
+        else:
+            print(f"Order failed: {response.error_code} - {response.error_message}")
+
+
+
+
+# set up API client
+username = 'your_betfair_username'
+password = 'your_betfair_password'
+app_key = 'your_betfair_app_key'
+certs_path = 'path_to_your_ssl_certificates'
+trading = betfairlightweight.APIClient(username, password, app_key, certs_path=certs_path)
+trading.login()
+
+# create MarketMaker instance
+market_filter = filters.market_filter(event_ids=['1234567'], market_types=['MATCH_ODDS'])
+market_books = trading.betting.list_market_book(market_filter=market_filter)
+batb = market_books[0].runners[0].ex.available_to_back
+batl = market_books[0].runners[0].ex.available_to_lay
+market_maker = MarketMaker(trading, batb, batl)
+
+# place orders
+market_maker.make_market(num_ticks=5)
+
+# logout
+trading.logout()
---
+++ sim.py
@@ -0,0 +1,526 @@
+import numpy as np
+import pandas as pd
+# Constants
+global col_row_names, col_row_names2, col_row_names3,col_row_names4
+col_row_names = ["0-0","0-15","15-0","15-15","30-0","0-30","40-0","30-15","15-30","0-40","40-15","15-40","30-30(DEUCE)","40-30(A-40)","30-40(40-A)","Win", "Lose"]
+col_row_names2 = ["0-0","0-1","1-0","1-1",
+                    "2-0","0-2","3-0","2-1",
+                    "1-2","0-3","4-0","3-1",
+                    "2-2","1-3","0-4","5-0",
+                    "4-1", "3-2","2-3","1-4",
+                    "0-5","5-1","4-2","3-3",
+                    "2-4","1-5","5-2","4-3","3-4",
+                    "2-5","5-3","4-4","3-5","5-4",
+                    "4-5", "5-5","6-5","5-6",
+                    "6-6","SETv1","SETv2","6-0",
+                    "6-1","6-2","6-3","6-4","4-6",
+                    "3-6","2-6","1-6","0-6","7-7","7-6","6-7"]
+col_row_names3 = ["0-0","0-1","1-0","1-1",
+                     "2-0","0-2","3-0","2-1",
+                     "1-2","0-3","4-0","3-1",
+                     "2-2","1-3","0-4","5-0",
+                     "4-1", "3-2","2-3","1-4",
+                     "0-5","5-1","4-2","3-3",
+                     "2-4","1-5","5-2","4-3","3-4",
+                     "2-5","5-3","4-4","3-5","5-4",
+                     "4-5", "5-5","6-5","5-6",
+                     "6-6","SETv1","SETv2"]
+col_row_names4 = ["0-0","0-1","1-0","1-1","2-0","0-2","2-1","1-2","V1","V2"]
+matrix = np.zeros((1,17))
+game_initial_state = pd.DataFrame(data=matrix,columns = col_row_names)
+game_initial_state.at[0,"0-0"]= int(1)
+matrix = np.zeros((1,54))
+tb_initial_state = pd.DataFrame(data=matrix,columns=col_row_names2)
+tb_initial_state.at[0,"0-0"] = 1
+matrix = np.zeros((1,41))
+set_initial_sate = pd.DataFrame(data=matrix,columns=col_row_names3)
+set_initial_sate.at[0,"0-0"] = 1
+matrix = np.zeros((1,10))
+match_initial_state = pd.DataFrame(data = matrix,columns=col_row_names4)#
+match_initial_state.at[0,"0-0"] = 1
+
+
+
+def game_trans_matrix(ppoint_server):
+    ppoint_ret = 1 - ppoint_server
+    matrix = np.zeros((17,17))
+    tMat1 = pd.DataFrame(data = matrix, index=col_row_names,columns=col_row_names)
+    tMat1.at["0-0","15-0"] = ppoint_server
+    tMat1.at["15-0","30-0"] = ppoint_server
+    tMat1.at["0-15","15-15"] = ppoint_server
+    tMat1.at["30-0","40-0"] = ppoint_server
+    tMat1.at["15-15","30-15"] = ppoint_server
+    tMat1.at["0-30","15-30"] = ppoint_server
+    tMat1.at["40-0","Win"] = ppoint_server
+    tMat1.at["30-15","40-15"] = ppoint_server
+    tMat1.at["40-15","Win"] = ppoint_server
+    tMat1.at["40-30(A-40)","Win"] = ppoint_server
+    tMat1.at["0-40","15-40"] = ppoint_server
+    tMat1.at["15-40","30-40(40-A)"] = ppoint_server
+    tMat1.at["30-40(40-A)","30-30(DEUCE)"] = ppoint_server
+    tMat1.at["15-30","30-30(DEUCE)"] = ppoint_server
+    tMat1.at["30-30(DEUCE)","40-30(A-40)"] = ppoint_server
+
+    tMat1.at["0-0","0-15"] = ppoint_ret
+    tMat1.at["15-0","15-15"] = ppoint_ret
+    tMat1.at["0-15","0-30"] = ppoint_ret
+    tMat1.at["30-0","30-15"] = ppoint_ret
+    tMat1.at["15-15","15-30"] = ppoint_ret
+    tMat1.at["0-30","0-40"] = ppoint_ret
+    tMat1.at["40-0","40-15"] = ppoint_ret
+    tMat1.at["30-15","30-30(DEUCE)"] = ppoint_ret
+    tMat1.at["40-15","40-30(A-40)"] = ppoint_ret
+    tMat1.at["40-30(A-40)","30-30(DEUCE)"] = ppoint_ret
+    tMat1.at["0-40","Lose"] = ppoint_ret
+    tMat1.at["15-40","Lose"] = ppoint_ret
+    tMat1.at["30-40(40-A)","Lose"] = ppoint_ret
+    tMat1.at["15-30","15-40"] = ppoint_ret
+    tMat1.at["30-30(DEUCE)","30-40(40-A)"] = ppoint_ret
+
+
+    tMat1.at["Lose","Lose"] = 1
+    tMat1.at["Win","Win"] = 1
+
+
+    return tMat1
+
+def prob_game(ppoint_server,s_game):
+    matrix = game_trans_matrix(ppoint_server)
+    temp = matrix
+    for i in range(50):
+        matrix = np.dot(matrix,matrix)
+    matrix = pd.DataFrame(data = matrix, index=col_row_names,columns=col_row_names)
+    probs = np.dot(s_game,matrix)
+    return probs
+    
+def tie_break(ppoint_srv1, ppoint_srv2):
+    states = ["0-0","0-1","1-0","1-1","2-0","0-2","3-0","2-1",
+             "1-2","0-3","4-0","3-1",
+             "2-2","1-3","0-4","5-0",
+             "4-1", "3-2","2-3","1-4",
+             "0-5","5-1","4-2","3-3",
+             "2-4","1-5","5-2","4-3","3-4",
+             "2-5","5-3","4-4","3-5","5-4",
+             "4-5", "5-5","6-5","5-6",
+             "6-6","SETv1","SETv2","6-0",
+             "6-1","6-2","6-3","6-4","4-6",
+             "3-6","2-6","1-6","0-6","7-7","7-6","6-7"]
+    matrix = np.zeros((54,54))
+    tMat2 = pd.DataFrame(data = matrix, index=states,columns=states)
+    tMat2.at["0-0","1-0"] = ppoint_srv1
+    tMat2.at["3-0","4-0"] = ppoint_srv1
+    tMat2.at["2-1","3-1"] = ppoint_srv1
+    tMat2.at["1-2","2-2"] = ppoint_srv1
+    tMat2.at["0-3","1-3"]  = ppoint_srv1
+    tMat2.at["4-0","5-0"] = ppoint_srv1
+    tMat2.at["3-1","4-1"] = ppoint_srv1
+    tMat2.at["2-2","3-2"] = ppoint_srv1
+    tMat2.at["1-3","2-3"] = ppoint_srv1
+    tMat2.at["0-4","1-4"] = ppoint_srv1
+    tMat2.at["6-1","SETv1"] = ppoint_srv1
+    tMat2.at["5-2","6-2"] = ppoint_srv1
+    tMat2.at["4-3","5-3"] = ppoint_srv1
+    tMat2.at["3-4","4-4"]  = ppoint_srv1
+    tMat2.at["2-5","3-5"] = ppoint_srv1
+    tMat2.at["1-6","2-6"] = ppoint_srv1
+    tMat2.at["6-2","SETv1"] = ppoint_srv1
+    tMat2.at["5-3","6-3"] = ppoint_srv1
+    tMat2.at["4-4","5-4"] = ppoint_srv1
+    tMat2.at["3-5","4-5"]  = ppoint_srv1
+    tMat2.at["2-6","3-6"] = ppoint_srv1
+    tMat2.at["6-5","SETv1"] = ppoint_srv1
+    tMat2.at["5-6","6-6"] = ppoint_srv1
+    tMat2.at["6-6","7-6"] = ppoint_srv1
+
+    tMat2.at["0-0","0-1"] = 1 - ppoint_srv1
+    tMat2.at["3-0","3-1"] = 1 - ppoint_srv1
+    tMat2.at["2-1","2-2"] = 1 - ppoint_srv1
+    tMat2.at["1-2","1-3"] = 1 - ppoint_srv1
+    tMat2.at["0-3","0-4"]  = 1 - ppoint_srv1
+    tMat2.at["4-0","4-1"] = 1 - ppoint_srv1
+    tMat2.at["3-1","3-2"] = 1 - ppoint_srv1
+    tMat2.at["2-2","2-3"] = 1 - ppoint_srv1
+    tMat2.at["1-3","1-4"] = 1 - ppoint_srv1
+    tMat2.at["0-4","0-5"] = 1 - ppoint_srv1
+    tMat2.at["6-1","6-2"] = 1 - ppoint_srv1
+    tMat2.at["5-2","5-3"] = 1 - ppoint_srv1
+    tMat2.at["4-3","4-4"] = 1 - ppoint_srv1
+    tMat2.at["3-4","3-5"]  = 1 - ppoint_srv1
+    tMat2.at["2-5","2-6"] = 1 - ppoint_srv1
+    tMat2.at["1-6","SETv2"] = 1 - ppoint_srv1
+    tMat2.at["6-2","6-3"] = 1 - ppoint_srv1
+    tMat2.at["5-3","5-4"] = 1 - ppoint_srv1
+    tMat2.at["4-4","4-5"] = 1 - ppoint_srv1
+    tMat2.at["3-5","3-6"]  = 1 - ppoint_srv1
+    tMat2.at["2-6","SETv2"] = 1 - ppoint_srv1
+    tMat2.at["6-5","6-6"] = 1 - ppoint_srv1
+    tMat2.at["5-6","SETv2"] = 1 - ppoint_srv1
+    tMat2.at["3-4","3-5"] = 1 - ppoint_srv1
+    tMat2.at["6-6","6-7"] = 1 - ppoint_srv1
+    
+    tMat2.at["1-0","1-1"] = ppoint_srv2
+    tMat2.at["0-1","0-2"] = ppoint_srv2
+    tMat2.at["2-0","2-1"] = ppoint_srv2
+    tMat2.at["1-1","1-2"] = ppoint_srv2
+    tMat2.at["0-2","0-3"]  = ppoint_srv2
+    tMat2.at["0-3","0-4"] = ppoint_srv2
+    tMat2.at["5-0","5-1"] = ppoint_srv2
+    tMat2.at["4-1","4-2"] = ppoint_srv2
+    tMat2.at["3-2","3-3"] = ppoint_srv2
+    tMat2.at["2-3","2-4"] = ppoint_srv2
+    tMat2.at["1-4","1-5"] = ppoint_srv2
+    tMat2.at["0-5","0-6"] = ppoint_srv2
+    tMat2.at["6-0","6-1"] = ppoint_srv2
+    tMat2.at["5-1","5-2"]  = ppoint_srv2
+    tMat2.at["4-2","5-2"] = ppoint_srv2
+    tMat2.at["3-3","3-4"] = ppoint_srv2
+    tMat2.at["2-4","2-5"] = ppoint_srv2
+    tMat2.at["1-5","1-6"] = ppoint_srv2
+    tMat2.at["0-6","SETv2"] = ppoint_srv2
+    tMat2.at["6-3","6-4"] = ppoint_srv2
+    tMat2.at["5-4","5-5"] = ppoint_srv2
+    tMat2.at["4-5","4-6"] = ppoint_srv2
+    tMat2.at["3-6","SETv2"] = ppoint_srv2
+    tMat2.at["6-4","6-5"] = ppoint_srv2
+    tMat2.at["5-5","5-6"] = ppoint_srv2
+    tMat2.at["4-6","SETv2"] = ppoint_srv2
+    tMat2.at["6-7","SETv2"] = ppoint_srv2
+    tMat2.at["7-6","7-7"] = ppoint_srv2
+    tMat2.at["4-2","4-3"] = ppoint_srv2
+    tMat2.at["7-7","5-6"] = ppoint_srv2
+
+    tMat2.at["1-0","2-0"] = 1 - ppoint_srv2
+    tMat2.at["0-1","1-1"] = 1 - ppoint_srv2
+    tMat2.at["2-0","3-0"] = 1 - ppoint_srv2
+    tMat2.at["1-1","2-1"] = 1 - ppoint_srv2
+    tMat2.at["0-2","1-2"]  = 1 - ppoint_srv2
+    tMat2.at["0-3","1-3"] = 1 - ppoint_srv2
+    tMat2.at["5-0","6-0"] = 1 - ppoint_srv2
+    tMat2.at["4-1","5-1"] = 1 - ppoint_srv2
+    tMat2.at["3-2","4-2"] = 1 - ppoint_srv2
+    tMat2.at["2-3","3-3"] = 1 - ppoint_srv2
+    tMat2.at["1-4","2-4"] = 1 - ppoint_srv2
+    tMat2.at["0-5","1-5"] = 1 - ppoint_srv2
+    tMat2.at["6-0","SETv1"] = 1 - ppoint_srv2
+    tMat2.at["5-1","6-1"]  = 1 - ppoint_srv2
+    tMat2.at["4-2","5-2"] = 1 - ppoint_srv2
+    tMat2.at["3-3","4-3"] = 1 - ppoint_srv2
+    tMat2.at["2-4","3-4"] = 1 - ppoint_srv2
+    tMat2.at["1-5","2-5"] = 1 - ppoint_srv2
+    tMat2.at["0-6","1-6"] = 1 - ppoint_srv2
+    tMat2.at["6-3","SETv1"] = 1 - ppoint_srv2
+    tMat2.at["5-4","6-4"] = 1 - ppoint_srv2
+    tMat2.at["4-5","5-5"] = 1 - ppoint_srv2
+    tMat2.at["3-6","4-6"] = 1 - ppoint_srv2
+    tMat2.at["6-4","SETv1"] = 1 - ppoint_srv2
+    tMat2.at["5-5","6-5"] = 1 - ppoint_srv2
+    tMat2.at["4-6","5-6"] = 1 - ppoint_srv2
+    tMat2.at["3-6","4-6"] = 1 - ppoint_srv2
+    tMat2.at["6-7","7-7"] = 1 - ppoint_srv2
+    tMat2.at["7-6","SETv1"] = 1 - ppoint_srv2
+    tMat2.at["7-7","6-5"] = 1 - ppoint_srv2
+    
+    # Set stationary states
+    tMat2.at["SETv1","SETv1"] = 1
+    tMat2.at["SETv2","SETv2"] = 1
+    return tMat2
+
+
+def prob_tie(ppoint_srv1,ppoint_srv2,s_tb):
+    matrix = tie_break(ppoint_srv1,ppoint_srv2)
+    for i in range(1000):
+        matrix = np.dot(matrix,matrix)
+    matrix = pd.DataFrame(data = matrix, index=col_row_names2,columns=col_row_names2)
+    probs = np.dot(s_tb,matrix)
+    return probs
+    
+def set(pwin1,pwin2,ptie1):
+    matrix = np.zeros((41,41))
+    tMat3 = pd.DataFrame(data = matrix, index=col_row_names3,columns=col_row_names3)
+    tMat3.at["0-0","1-0"] = pwin1
+    tMat3.at["2-0","3-0"] = pwin1
+    tMat3.at["1-1","2-1"] = pwin1
+    tMat3.at["0-2","1-2"] = pwin1
+    tMat3.at["4-0","5-0"]  = pwin1
+    tMat3.at["3-1","4-1"] = pwin1
+    tMat3.at["2-2","3-2"] = pwin1
+    tMat3.at["1-3","2-3"] = pwin1
+    tMat3.at["0-4","1-4"] = pwin1
+    tMat3.at["5-1","SETv1"] = pwin1
+    tMat3.at["4-2","5-2"] = pwin1
+    tMat3.at["3-3","4-3"] = pwin1
+    tMat3.at["2-4","3-4"] = pwin1
+    tMat3.at["1-5","2-5"]  = pwin1
+    tMat3.at["5-3","SETv1"] = pwin1
+    tMat3.at["4-4","5-4"] = pwin1
+    tMat3.at["3-5","4-5"] = pwin1
+    tMat3.at["5-5","6-5"] = pwin1
+    
+    tMat3.at["0-0","0-1"] = 1 - pwin1
+    tMat3.at["2-0","2-1"] = 1 - pwin1
+    tMat3.at["1-1","1-2"] = 1 - pwin1
+    tMat3.at["0-2","0-3"] = 1 - pwin1
+    tMat3.at["4-0","4-1"]  = 1 - pwin1
+    tMat3.at["3-1","3-2"] = 1 - pwin1
+    tMat3.at["2-2","2-3"] = 1 - pwin1
+    tMat3.at["1-3","1-4"] = 1 - pwin1
+    tMat3.at["0-4","0-5"] = 1 - pwin1
+    tMat3.at["5-1","5-2"] = 1 - pwin1
+    tMat3.at["4-2","4-3"] = 1 - pwin1
+    tMat3.at["3-3","3-4"] = 1 - pwin1
+    tMat3.at["2-4","2-5"] = 1 - pwin1
+    tMat3.at["1-5","SETv2"]  = 1 - pwin1
+    tMat3.at["5-3","5-4"] = 1 - pwin1
+    tMat3.at["4-4","4-5"] = 1 - pwin1
+    tMat3.at["3-5","SETv2"] = 1 - pwin1
+    tMat3.at["5-5","5-6"] = 1 - pwin1
+    
+    tMat3.at["1-0","1-1"] = pwin2
+    tMat3.at["0-1","0-2"] = pwin2
+    tMat3.at["3-0","3-1"] = pwin2
+    tMat3.at["2-1","2-2"] = pwin2
+    tMat3.at["1-2","1-3"]  = pwin2
+    tMat3.at["0-3","0-4"] = pwin2
+    tMat3.at["5-0","5-1"] = pwin2
+    tMat3.at["4-1","4-2"] = pwin2
+    tMat3.at["3-2","3-3"] = pwin2
+    tMat3.at["2-3","2-4"] = pwin2
+    tMat3.at["1-4","1-5"] = pwin2
+    tMat3.at["0-5","SETv2"] = pwin2
+    tMat3.at["5-2","5-3"] = pwin2
+    tMat3.at["4-3","4-4"]  = pwin2
+    tMat3.at["3-4","3-5"] = pwin2
+    tMat3.at["2-5","SETv2"] = pwin2
+    tMat3.at["5-4","5-5"] = pwin2
+    tMat3.at["4-5","SETv2"] = pwin2
+    tMat3.at["5-6","SETv2"] = pwin2
+    tMat3.at["6-5","6-6"] = pwin2
+
+    tMat3.at["1-0","2-0"] = 1 - pwin2
+    tMat3.at["0-1","1-1"] = 1 - pwin2
+    tMat3.at["3-0","4-0"] = 1 - pwin2
+    tMat3.at["2-1","3-1"] = 1 - pwin2
+    tMat3.at["1-2","2-2"]  = 1 - pwin2
+    tMat3.at["0-3","1-3"] = 1 - pwin2
+    tMat3.at["5-0","SETv1"] = 1 - pwin2
+    tMat3.at["4-1","5-1"] = 1 - pwin2
+    tMat3.at["3-2","4-2"] = 1 - pwin2
+    tMat3.at["2-3","3-3"] = 1 - pwin2
+    tMat3.at["1-4","2-4"] = 1 - pwin2
+    tMat3.at["0-5","1-5"] = 1 - pwin2
+    tMat3.at["5-2","SETv1"] = 1 - pwin2
+    tMat3.at["4-3","5-3"]  = 1 - pwin2
+    tMat3.at["3-4","4-4"] = 1 - pwin2
+    tMat3.at["2-5","3-5"] = 1 - pwin2
+    tMat3.at["5-4","SETv1"] = 1 - pwin2
+    tMat3.at["4-5","5-5"] = 1 - pwin2
+    tMat3.at["5-6","6-6"] = 1 - pwin2
+    tMat3.at["6-5","SETv1"] = 1 - pwin2
+    
+    # Set stationary states
+    tMat3.at["SETv1","SETv1"] = 1
+    tMat3.at["SETv2","SETv2"] = 1
+    
+    # Set tie-break cases
+    tMat3.at["6-6","SETv1"] = ptie1
+    tMat3.at["6-6","SETv2"] = 1 - ptie1
+    return tMat3
+def prob_set(pwin1,pwin2,ptie1,s_set):
+    matrix = set(pwin1,pwin2,ptie1)
+    for i in range(1000):
+        matrix = np.dot(matrix,matrix)
+    matrix = pd.DataFrame(data = matrix, index=col_row_names3,columns=col_row_names3)
+    probs = np.dot(s_set,matrix)
+    return probs
+
+def match(pset_v1):
+    pset_v2 = 1-pset_v1
+    matrix = np.zeros((10,10))
+    tMat4 = pd.DataFrame(data = matrix, index=col_row_names4,columns=col_row_names4)
+    tMat4.at["0-0","1-0"] = pset_v1
+    tMat4.at["1-0","2-0"] = pset_v1
+    tMat4.at["0-1","1-1"] = pset_v1
+    tMat4.at["1-1","2-1"] = pset_v1
+    
+    tMat4.at["0-0","0-1"] = pset_v2
+    tMat4.at["1-0","1-1"] = pset_v2
+    tMat4.at["0-1","0-2"] = pset_v2
+    tMat4.at["1-1","1-2"] = pset_v2
+    
+    # Set stationary states
+    tMat4.at["2-0","2-0"] = 1
+    tMat4.at["2-1","2-1"] = 1
+    tMat4.at["0-2","0-2"] = 1
+    tMat4.at["1-2","1-2"] = 1
+    
+    tMat4.at["V1","V1"] = 1
+    tMat4.at["V2","V2"] = 1
+    print(tMat4.shape)
+    return tMat4
+
+def prob_match(pset_v1,s_match):
+    matrix = match(pset_v1)
+    for i in range(1000):
+        matrix = np.dot(matrix,matrix)
+    matrix = pd.DataFrame(data = matrix, index=col_row_names4,columns=col_row_names4)
+    probs = np.dot(s_match,matrix)
+    return probs
+
+def predict1(gamescore, pwin1, pwin2, ptie1, pset_v1, match_initial_state, set_initial_sate, game_initial_state, tb_initial_state):
+    s1match = match_initial_state
+    s1set = set_initial_sate
+    s1game = game_initial_state
+    s1tb = tb_initial_state
+    s1set.at[0, "0-0"] = 0
+    s1set.at[0, gamescore] = 1
+
+    ans = prob_set( pwin1, pwin2, ptie1, s1set)
+    ans = pd.DataFrame(data = ans, columns=col_row_names3)
+    temp= ans.at[0,"SETv1"]
+
+    ans = prob_set( pwin1, pwin2, ptie1, s1set)
+    ans = pd.DataFrame(data = ans, columns=col_row_names3)
+    temp1= ans.at[0,"SETv2"]
+
+    s1match.at[0,"1-0"] = temp
+    s1match.at[0,"0-1"] = temp1
+    s1match.at[0, "0-0"] = 0
+    
+    prob_entire_match = prob_match(pset_v1, s1match)
+    
+    return prob_entire_match
+
+
+def predict2(setscore, gamescore, pwin1, pwin2, ptie1, pset_v1, match_initial_state, set_initial_sate, game_initial_state, tb_initial_state):
+    s1match = match_initial_state
+    s1set = set_initial_sate
+    s1game = game_initial_state
+    s1tb = tb_initial_state
+    
+    s1match.at[0, "0-0"] = 0
+    s1match.at[0, setscore] = 1
+    s1set.at[0, "0-0"] = 0
+    s1set.at[0, gamescore] = 1
+
+    if (setscore == "1-0"):
+        ans = prob_set( pwin1, pwin2, ptie1, s1set)
+        ans = pd.DataFrame(data = ans, columns=col_row_names3)
+        temp= ans.at[0,"SETv1"]
+
+        ans = prob_set( pwin1, pwin2, ptie1, s1set)
+        ans = pd.DataFrame(data = ans, columns=col_row_names3)
+        temp1= ans.at[0,"SETv2"]
+
+        s1match.at[0,"2-0"] = temp
+        s1match.at[0,"1-1"] = temp1
+        s1match.at[0, "1-0"] = 0
+        
+        
+        prob_entire_match = prob_match(pset_v1, s1match)
+
+    if (setscore == "0-1"):
+        ans = prob_set( pwin1, pwin2, ptie1, s1set)
+        ans = pd.DataFrame(data = ans, columns=col_row_names3)
+        temp= ans.at[0,"SETv2"]
+
+        ans = prob_set( pwin1, pwin2, ptie1, s1set)
+        ans = pd.DataFrame(data = ans, columns=col_row_names3)
+        temp1= ans.at[0,"SETv1"]
+
+        s1match.at[0,"0-2"] = temp
+        s1match.at[0,"1-1"] = temp1
+        s1match.at[0, "0-1"] = 0
+        
+        
+        prob_entire_match = prob_match(pset_v1, s1match)
+    
+
+    return prob_entire_match
+def predict3(gamescore, pwin1, pwin2, ptie1, pset_v1, match_initial_state, set_initial_sate, game_initial_state, tb_initial_state):
+    s1match = match_initial_state
+    s1set = set_initial_sate
+    s1game = game_initial_state
+    s1tb = tb_initial_state
+    
+    setscore = "1-1"
+    
+    s1match[0, "0-0"] = 0
+    s1match[0, setscore] = 1
+    s1set[0, "0-0"] = 0
+    s1set[0, gamescore] = 1
+
+    ans = prob_set( pwin1, pwin2, ptie1, s1set)
+    ans = pd.DataFrame(data = ans, columns=col_row_names3)
+    temp= ans.at[0,"SETv1"]
+
+    ans = prob_set( pwin1, pwin2, ptie1, s1set)
+    ans = pd.DataFrame(data = ans, columns=col_row_names3)
+    temp1= ans.at[0,"SETv2"]
+
+    s1match.at[0,"2-1"] = temp
+    s1match.at[0,"1-2"] = temp1
+    s1match.at[0, "1-1"] = 0
+
+    prob_entire_match = prob_match(pset_v1,s1match)
+    return prob_entire_match
+
+def tennis_model(ppoint_srv1, ppoint_srv2, setscore, gamescore, match_initial_state, set_initial_sate, game_initial_state, tb_initial_state):
+    ans = prob_game(ppoint_srv1, game_initial_state)
+    ans = pd.DataFrame(data = ans, columns=col_row_names)
+    temp = ans.at[0,"Win"]
+    ans = prob_game(ppoint_srv2, game_initial_state)
+    ans = pd.DataFrame(data = ans, columns=col_row_names)
+    temp1 = ans.at[0,"Win"]
+    ans = prob_tie(ppoint_srv1,ppoint_srv2,tb_initial_state)
+    ans = pd.DataFrame(data = ans, columns=col_row_names2)
+    temp2= ans.at[0,"SETv1"]
+    ans = prob_set(temp,temp1,temp2,set_initial_sate)
+    ans = pd.DataFrame(data = ans, columns=col_row_names3)
+    temp3= ans.at[0,"SETv1"]
+
+    if(setscore == "0-0"):
+        prob_entire_match = predict1(gamescore,temp,temp1,temp2,temp3,match_initial_state,set_initial_sate,game_initial_state,tb_initial_state)
+    if setscore == "1-1":
+        prob_entire_match = predict3(gamescore,temp,temp1,temp2,temp3,match_initial_state,set_initial_sate,game_initial_state,tb_initial_state)
+    if setscore == "1-0" or setscore == "0-1":
+        print("yes")
+        prob_entire_match = predict2(setscore,gamescore,temp,temp1,temp2,temp3,match_initial_state,set_initial_sate,game_initial_state,tb_initial_state)
+    return prob_entire_match
+
+
+
+
+
+
+
+
+
+
+    
+
+
+ans = prob_game(0.7,game_initial_state)
+ans = pd.DataFrame(data = ans, columns=col_row_names)#
+temp = ans.at[0,"Win"]
+ans = prob_game(0.6,game_initial_state)
+ans = pd.DataFrame(data = ans, columns=col_row_names)
+temp1 = ans.at[0,"Win"]
+ans = prob_tie(0.7,0.6,tb_initial_state)
+ans = pd.DataFrame(data = ans, columns=col_row_names2)
+temp2 = ans.at[0,"SETv1"]
+ans = prob_set(temp,temp1,temp2,set_initial_sate)
+ans = pd.DataFrame(data = ans, columns=col_row_names3)
+ans = prob_match(0.7,match_initial_state)
+ans = pd.DataFrame(data = ans, columns=col_row_names4)
+ans = tennis_model(0.7,0.6,"1-0","0-3",match_initial_state,set_initial_sate,game_initial_state,tb_initial_state)
+ans = pd.DataFrame(data = ans, columns=col_row_names4)
+
+print(ans)
+
+
+
+
+
+  
+
+
