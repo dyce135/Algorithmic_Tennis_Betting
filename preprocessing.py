1087b053a1466aaf1e1ff9a659045607ceb6954f
dyce135 <zceekc0@ucl.ac.uk>
2023-04-14 20:33:01 +0100

    updates

--- preprocessing.py
+++ preprocessing.py
@@ -9,69 +9,119 @@ from tensorflow.keras import layers
 # import markov_sim as markov
 import json
 import os
+from datetime import datetime
+from re import sub
+from dateutil import  parser
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['HSA_OVERRIDE_GFX_VERSION'] = '10.3.0'
 os.environ['LD_LIBRARY_PATH'] = '$LD_LIBRARY_PATH:/opt/rocm-5.3.0/lib'
 
-with open('1.209134319.json') as data_file:
-    data_dict = json.load(data_file)
+data_list = []
 
-data_dict = data_dict['mcm']
+for line in open('1.145414064.json', 'r'):
+    data_list.append(json.loads(line))
 
+runner_id = 2249834
+runner_id_2 = 2251410
+market_datetime = parser.parse(data_list[0]['mc'][0]['marketDefinition']['marketTime'])
+market_timestamp = datetime.timestamp(market_datetime) * 1000
 
-runner_id = 8859306
-runner_id_2 = 2249229
 
 def get_list(runner_id):
+    # Create list for each runner
     runner_list = []
-    for item in data_dict.items():
-        if 'rc' in item[1]:
-            temp_dict = {k: v for (k, v) in item[1]['rc'][0].items() if v == runner_id}
-            if temp_dict:
-                runner_list.append([item[1]['rc'][0], item[1]['got_packet_time']])
-            elif len(item[1]['rc']) > 1:
-                temp_dict_2 = {k: v for (k, v) in item[1]['rc'][1].items() if v == runner_id}
-                if temp_dict_2:
-                    runner_list.append([item[1]['rc'][1], item[1]['got_packet_time']])
+    for instance in data_list:
+        if instance['pt'] > market_timestamp:
+            if instance['mc'][0]['rc']:
+                # Check for runner id
+                temp_dict = {k: v for (k, v) in instance['mc'][0]['rc'][0].items() if v == runner_id}
+                if temp_dict:
+                    # Append runner info
+                    runner_list.append([instance['mc'][0]['rc'][0], instance['pt']])
+                elif len(instance['mc'][0]['rc']) > 1:
+                    # If more than one runner
+                    temp_dict_2 = {k: v for (k, v) in instance['mc'][0]['rc'][1].items() if v == runner_id}
+                    if temp_dict_2:
+                        runner_list.append([instance['mc'][0]['rc'][1], instance['pt']])
     return runner_list
 
-def convert_odds(runner_list, back_or_lay):
-    
+
+def convert_odds(runner_list):
+    # Convert to back/lay/last traded odds
+
     list = []
 
-    if back_or_lay == 0:
-        for item in runner_list:
-            if 'batb' in item[0]:
-                list.append([item[0]['batb'][0][1], item[1]])
-            elif 'bdatb' in item[0]:
-                list.append([item[0]['bdatb'][0][1], item[1]])
-    else:
-        for item in runner_list:
-            if 'batl' in item[0]:
-                list.append([item[0]['batl'][0][1], item[1]])
-            elif 'bdatl' in item[0]:
-                list.append([item[0]['bdatl'][0][1], item[1]])
-    
+    for item in runner_list:
+        if 'ltp' in item[0]:
+            list.append([item[0]['ltp'], item[1]])
+
+    del list[-1]
     arr = np.array(list)
     arr = arr[arr[:, 0] != 0]
-    arr[:, 1] = arr[:, 1] - arr[0, 1]
+    arr[:, 1] = arr[:, 1] - market_timestamp
     implied_odds = np.array([1 / arr[:, 0], arr[:, 1]]).T
-    
+
     return implied_odds
+
+
+# Find avg ltp odds
+def odds_avg(runner_1, runner_2):
+    if runner_1[-1, 1] > runner_2[-1, 1]:
+        shape = round(runner_1[-1, 1] / 100)
+    else:
+        shape = round(runner_2[-1, 1] / 100)
+
+    odds_1 = np.empty(shape)
+    odds_2 = np.empty(shape)
+
+    for element in runner_1:
+        odds_1[round(element[1] / 100) - 1] = element[0]
+
+    for element in runner_2:
+        odds_2[round(element[1] / 100) - 1] = element[0]
+
+    df = pd.DataFrame({'runner 1': odds_1, 'runner_2': odds_2})
+    print(df.head())
+    df.replace(0, np.nan, inplace=True)
+    df.interpolate(method='linear')
+    df['avg'] = df.mean(axis=1)
+    print(df.head())
+    avg = df['avg'].to_numpy()
+
+    return avg
+
+def calc_pup():
+    pass
+
+
+def get_current_spread():
+    pass
+
+
+def calc_future_spread():
+    pass
+
+
+def calc_score():
+    pass
+
+
+def get_markov_odds():
+    pass
+
 
 runner_list_1 = get_list(runner_id)
 runner_list_2 = get_list(runner_id_2)
-back_odds_1 = convert_odds(runner_list_1, back_or_lay=0)
-lay_odds_1 = convert_odds(runner_list_1, back_or_lay=1)
-back_odds_2 = convert_odds(runner_list_2, back_or_lay=0)
-lay_odds_2 = convert_odds(runner_list_2, back_or_lay=1)
+runner_odds_1 = convert_odds(runner_list_1)
+runner_odds_2 = convert_odds(runner_list_2)
+avg_odds = odds_avg(runner_odds_1, runner_odds_2)
 
-plt.plot(lay_odds_1[:, 1], lay_odds_1[:, 0])
+plt.plot(runner_odds_1[:, 1], runner_odds_1[:, 0])
+plt.show()
+plt.plot(runner_odds_2[:, 1], runner_odds_2[:, 0])
 plt.show()
 
-
-#
 # training_data_len = math.ceil(len(implied_odds) * 0.8)
 #
 # values = implied_odds.reshape(-1, 1)
