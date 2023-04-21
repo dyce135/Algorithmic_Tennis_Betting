a02e805ce109bf7de1285a811fca061cf1ca5889
dyce135 <zceekc0@ucl.ac.uk>
2023-04-21 12:22:46 +0100

    markov simulation amendments, seq2seq updates

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -12,31 +12,27 @@ os.environ['LD_LIBRARY_PATH'] = '$LD_LIBRARY_PATH:/opt/rocm-5.3.0/lib'
 
 def truncate(x, feature_cols=range(5), target_cols=range(5), train_len=825, test_len=165):
     in_, out_ = [], []
-    for i in range(len(x) - train_len-test_len+1):
+    for i in range(len(x) - train_len - test_len + 1):
         in_.append(x[i:(i + train_len), feature_cols].tolist())
-        out_.append(x[(i + train_len):(i + train_len+test_len), target_cols].tolist())
+        out_.append(x[(i + train_len):(i + train_len + test_len), target_cols].tolist())
 
     return np.array(in_), np.array(out_)
 
 
 def seq2seq(input_dim, output_dim, latent_dim, learning_rate=0.001):
-
     input_train = Input(shape=(input_dim[1], input_dim[2]))
     output_train = Input(shape=(output_dim[1], output_dim[2]))
-    encoder_last_h1, encoder_last_h2, encoder_last_c = LSTM(
+    encoder_last_h1, encoder_last_h2 = GRU(
         latent_dim, activation='elu', dropout=0.2, recurrent_dropout=0.2,
         return_sequences=False, return_state=True)(input_train)
-    encoder_last_h1 = BatchNormalization(momentum=0.3)(encoder_last_h1)
-    encoder_last_c = BatchNormalization(momentum=0.3)(encoder_last_c)
+    encoder_last_h1 = BatchNormalization(momentum=0.2)(encoder_last_h1)
     decoder = RepeatVector(output_train.shape[1])(encoder_last_h1)
-    decoder = LSTM(latent_dim, activation='elu', dropout=0.2, recurrent_dropout=0.2, return_state=False,
-                   return_sequences=True)(
-        decoder, initial_state=[encoder_last_h1, encoder_last_c])
+    decoder = GRU(latent_dim, activation='elu', dropout=0.2, recurrent_dropout=0.2, return_state=False,
+                  return_sequences=True)(
+        decoder, initial_state=[encoder_last_h1])
     out = TimeDistributed(Dense(output_train.shape[2]))(decoder)
     model = Model(inputs=input_train, outputs=out)
     opt = Adam(lr=learning_rate, clipnorm=1)
     model.compile(loss='mse', optimizer=opt, metrics=['mae'])
 
     return model
-
-
